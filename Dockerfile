0c5d213093865de23b99cc16001fb2bed4a80561
kav1nsky <kav1nsky@yandex-team.ru>
2020-05-19 19:35:38 +0300

    Fix small bug, add stock market data monitoring

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.6
 
 RUN apt-get update && apt-get install -y vim curl jq
-RUN pip3 install selenium bs4 graphyte
+RUN pip3 install selenium bs4 graphyte requests
 RUN mkdir /code
 COPY wait-for-grid.sh /code/wait-for-grid.sh
 COPY run.py /code/run.py
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import logging
 import time
 import unicodedata
 import graphyte
-
+import requests
 
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -12,6 +12,8 @@ logging.getLogger().setLevel(logging.INFO)
 
 BASE_URL = 'https://yandex.ru/'
 
+MOEX_URL = 'https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities'
+
 CURRENCIES = {
     'USD': 'usd',
     'EUR': 'euro',
@@ -19,6 +21,7 @@ CURRENCIES = {
     'EUR MOEX': 'euro',
     'Нефть': 'oil'
 }
+
 
 def parse_yandex_page(page):
     currency_blocks = page.findAll('div', {'class': 'inline-stocks__item'})
@@ -37,16 +40,37 @@ def parse_yandex_page(page):
 
 GRAPHITE_HOST = 'graphite'
 
+
 def send_metrics(currencies):
     sender = graphyte.Sender(GRAPHITE_HOST, prefix='currencies')
     for currency in currencies:
         sender.send(currency[0], currency[1])
 
-def main():
 
+def get_market_data(security):
+    result = {}
+    data = requests.get(f'{MOEX_URL}/{security}.json').json()
+    data = data['marketdata']
+    for i, column in enumerate(data['columns']):
+        if data['data'][0][i]:
+            try:
+                result[column] = float(data['data'][0][i])
+            except:
+                pass
+    return result
+
+
+def send_market_data(name, data):
+    sender = graphyte.Sender(GRAPHITE_HOST, prefix=name)
+    for k, v in data.items():
+        sender.send(k, float(v))
+
+
+def main():
     driver = webdriver.Remote(
         command_executor='http://selenium:4444/wd/hub',
-        desired_capabilities={'browserName': 'chrome', 'javascriptEnabled': True}
+        desired_capabilities={'browserName': 'chrome',
+                              'javascriptEnabled': True}
     )
 
     driver.get('https://yandex.ru')
@@ -57,10 +81,14 @@ def main():
     metric = parse_yandex_page(soup)
     send_metrics(metric)
 
+    data = get_market_data("YNDX")
+    send_market_data("yndx_data", data)
+
+
     driver.quit()
 
     logging.info('Accessed %s ..', BASE_URL)
-    logging.info('Page title: %s', driver.title)
+    logging.info('Page title: %s', soup.title)
 
 if __name__ == '__main__':
     main()
