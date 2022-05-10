c2f1e4b66fbe042faceb77d2ac325fcb7e8033f8
aschirkov <CHIRKOV.A.SERGE@SBERBANK.RU>
2022-05-11 02:38:41 +0300

    Добавил отправку метрик в инфлукс

--- run.py
+++ run.py
@@ -4,12 +4,16 @@ import time
 import unicodedata
 import graphyte
 
+from datetime import datetime
 
 from selenium import webdriver
+from influxdb import InfluxDBClient
 from bs4 import BeautifulSoup
 
 logging.getLogger().setLevel(logging.INFO)
 
+GRAPHITE_HOST = 'graphite'
+INFLUX_HOST = 'influxdb'
 BASE_URL = 'https://yandex.ru/'
 
 CURRENCIES = {
@@ -42,6 +46,27 @@ def send_metrics(currencies):
     for currency in currencies:
         sender.send(currency[0], currency[1])
 
+
+def send_influx(currencies):
+    print(f"send_influx: {currencies}")
+    client = InfluxDBClient(host=INFLUX_HOST, port=8086)
+    client.create_database('pyexample')
+    influx_body = []
+    metric_time = datetime.now().isoformat()
+    for cur_tittle, cur_value in currencies:
+        influx_body.append({
+            "measurement": "currencies",
+            "tags": {
+                "tittle": cur_tittle,
+            },
+            "time": metric_time,
+            "fields": {
+                "value": cur_value
+            }
+        })
+    client.write_points(influx_body, database='pyexample')
+
+
 def main():
 
     driver = webdriver.Remote(
@@ -56,6 +81,7 @@ def main():
 
     metric = parse_yandex_page(soup)
     send_metrics(metric)
+    send_influx(metric)
 
 
     logging.info('Accessed %s ..', BASE_URL)
